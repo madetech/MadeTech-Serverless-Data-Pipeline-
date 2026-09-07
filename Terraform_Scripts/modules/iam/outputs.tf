6ecf98f94f63c4611b68489ad8571152645a1f31
Mohamed Amin <mohamed.amiin@madetech.com>
2026-09-07 15:51:10 +0100

    add lambda roles

--- Terraform_Scripts/modules/iam/outputs.tf
+++ Terraform_Scripts/modules/iam/outputs.tf
@@ -1,3 +1,10 @@
+output "extraction_lambda_role_arn" {
+  value = aws_iam_role.extraction_lambda.arn
+}
+
+output "transformation_lambda_role_arn" {
+  value = aws_iam_role.transformation_lambda.arn
+}
 output "s3_access_policy_arn" {
   value = aws_iam_policy.s3_access.arn
 }
